499ac75d8fcf8de33d9f22e2ef25c98add6a3462
Quintin Henn <quintin.henn@gmail.com>
2021-03-31 07:53:46 +0200

    Progress on How-to-Make-a-2D-Game-in-Unity

---
+++ How-to-Make-a-2D-Game-in-Unity/source/Pirate-Game/Assets/Scripts/MovableObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour
+{
+    public float movementSpeed = 125F;
+
+    Rigidbody2D body2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body2D = GetComponent<Rigidbody>();
+
+        if (body2D == null) {
+            Debug.error("Rigidbody2D required for MovableObject script");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
