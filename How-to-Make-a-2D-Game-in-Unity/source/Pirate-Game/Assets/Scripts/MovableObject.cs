37337d9c7b2b511ae5b707f66cadbfc85232ed7c
Quintin Henn <quintin.henn@gmail.com>
2021-04-15 20:45:33 +0200

    Progress on How-to-Make-a-2D-Game-in-Unity/ BitDigree course

--- How-to-Make-a-2D-Game-in-Unity/source/Pirate-Game/Assets/Scripts/MovableObject.cs
+++ How-to-Make-a-2D-Game-in-Unity/source/Pirate-Game/Assets/Scripts/MovableObject.cs
@@ -11,16 +11,30 @@ public class MovableObject : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        body2D = GetComponent<Rigidbody>();
+        body2D = GetComponent<Rigidbody2D>();
 
         if (body2D == null) {
-            Debug.error("Rigidbody2D required for MovableObject script");
+            Debug.LogError("Rigidbody2D required for MovableObject script");
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
+        // This turns our mouse point into usable 2D coordinates
+        Vector3 touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 shipPosition = transform.position;
 
+        Vector2 directionToMove = new Vector2(0, 0);
+
+        if (shipPosition.y < touchPoint.y) {
+            // Need to move up, set the direction to move to be
+            // the distance between cursor and ship
+            directionToMove.y += touchPoint.y - shipPosition.y;
+        } else if (shipPosition.y > touchPoint.y) {
+            directionToMove.y -= shipPosition.y - touchPoint.y;
+        }
+
+        body2D.AddForce(directionToMove * movementSpeed, ForceMode2D.Force);
     }
+
 }
