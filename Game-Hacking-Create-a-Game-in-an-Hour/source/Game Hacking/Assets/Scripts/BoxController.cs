9933b4c2dccaac40745cee31eba543fafbc17ec6
Quintin Henn <quintin.henn@gmail.com>
2021-05-25 08:56:53 +0200

    Create introduction Scene

---
+++ Game-Hacking-Create-a-Game-in-an-Hour/source/Game Hacking/Assets/Scripts/BoxController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxController : MonoBehaviour
+{
+    public float speed = 0;
+    public float strafe_speed = 0;
+
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float controlled_walk_speed = Input.GetAxis("Horizontal") * speed;
+        float controlled_strafe_speed = Input.GetAxis("Vertical") * strafe_speed;
+        rigidbody.AddForce(controlled_walk_speed, 0, controlled_strafe_speed);
+    }
+}
