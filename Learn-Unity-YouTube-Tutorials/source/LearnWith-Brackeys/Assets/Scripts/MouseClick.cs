700a27564cf5e6e04cc4c5163942222c40af13c5
Quintin Henn <quintin.henn@gmail.com>
2022-01-12 11:00:39 +0100

    feat: learning screen shake and postprocessing

---
+++ Learn-Unity-YouTube-Tutorials/source/LearnWith-Brackeys/Assets/Scripts/MouseClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZCameraShake;
+
+public class MouseClick : MonoBehaviour
+{
+    // public CameraShake cameraShake;
+
+    void Update(){
+        if (Input.GetMouseButtonDown(0)) {
+            // StartCoroutine(cameraShake.Shake(0.15f, 0.4f));
+            CameraShaker.Instance.ShakeOnce(4.0f, 4.0f, 0.1f, 1.0f);
+        }
+    }
+}
